d856550a8f65f496ab92782aaeecc5181a773dff
Koromonnnnnnnn <764363@dpsk12.net>
2025-02-12 09:42:56 -0700

    .

---
+++ main.py
@@ -0,0 +1,25 @@
+import sys
+import pygame
+
+pygame.init()
+pygame.display.set_caption("bullet hell")
+screen = pygame.display.set_mode((800, 800))
+clock = pygame.time.Clock()
+
+def main():
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+    
+    screen.fill((0,0,0))
+
+    pygame.display.flip()
+
+
+pygame.quit()
+sys.exit()
+
+if __name__ == "__main__":
+    main()
